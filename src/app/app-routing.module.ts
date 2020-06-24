98b6537aa12c9930a80cf3c126a8ac1cbcef29ef
mohit dhakad <dhakadmohit463@gmail.com>
2020-06-24 14:42:24 +0530

    Initial Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PropertyComponent } from './property/property.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"property",component:PropertyComponent},
+  {path:"navbar",component:NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
