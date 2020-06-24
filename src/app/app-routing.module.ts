1cd9d5459ddf0cf1c160a200393bb2701efcfbc1
mohit dhakad <dhakadmohit463@gmail.com>
2020-06-24 16:03:26 +0530

    adding cards with images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PropertyComponent } from './property/property.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { ResidentialComponent } from './residential/residential.component';
+import { InvestmentComponent } from './investment/investment.component';
 
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"property",component:PropertyComponent},
-  {path:"navbar",component:NavbarComponent}
+  {path:"navbar",component:NavbarComponent},
+  {path:"residential",component:ResidentialComponent},
+  {path:"investment",component:InvestmentComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PropertyComponent } from './property/property.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ResidentialComponent } from './residential/residential.component';
+import { InvestmentComponent } from './investment/investment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PropertyComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResidentialComponent,
+    InvestmentComponent
   ],
   imports: [
     BrowserModule,
